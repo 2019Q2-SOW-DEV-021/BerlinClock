35af854da1af294824c96e3f8762fc7e49b3e8f5
2019-Q2_SOW_DEV_021 <2019q2sAowdev021@gmail.com>
2019-06-10 15:14:35 +0200

    Added Display name for test case

--- src/test/java/com/channels/kata/BerlinClockConverterTest.java
+++ src/test/java/com/channels/kata/BerlinClockConverterTest.java
@@ -1,14 +1,17 @@
 package com.channels.kata;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@DisplayName("Berlin Clock Converter")
 class BerlinClockConverterTest {
 
     @Test
-    public void testBerlinClockConverterToCheckNullInputValues() {
+    @DisplayName("Test for a null input value")
+    public void testBerlinClockConverterToCheckNullInputValue() {
         Exception exception = assertThrows(IllegalArgumentException.class, () ->
                 new BerlinClockConverter(null));
 
